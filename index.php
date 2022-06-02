ade702eb4a84f0519a1992bf1c52092d33fd3bf2
souka99 <soukainasoso13@gmail.com>
2022-06-02 11:24:21 +0100

    this is my first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>my_first</title>
+</head>
+<body>
+<center>
+	<h1>soukaina el azaar</h1>
+</center>
+</body>
+</html>
